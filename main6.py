a6e1ef84047ebf02b8fbac06b76a9eb6953019d1
Hamza Ali <hali361@mtroyal.ca>
2026-09-22 17:13:25 -0600

    Integrate loading statistics and JSON output

--- main6.py
+++ main6.py
@@ -1,10 +1,12 @@
-from file_IO import load_from_html
+from file_IO import load_data, save_to_json
 from data_processing import print_stats
-
 
 # load data
 filename = 'student_dataset.txt'
-table = load_from_html(filename)
+table = load_data(filename)
 
 # print table statistics
 print_stats(table)
+
+#save to json
+save_to_json(table, 'output.json')
